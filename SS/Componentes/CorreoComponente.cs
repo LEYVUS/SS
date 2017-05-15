d13bbc8fca1a45245edde2be40867656be266354
EduardoCapi <eduardo.capistran@uabc.edu.mx>
2017-05-15 16:08:37 -0700

    correo

---
+++ SS/Componentes/CorreoComponente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Mail;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SS.Componentes
+{
+    public class CorreoComponente
+    {
+
+        SmtpClient server = new SmtpClient("smtp.gmail.com", 587);
+
+        public CorreoComponente(String correo, String contrasenia)
+        {
+            server.Credentials = new System.Net.NetworkCredential(correo, contrasenia);
+            server.EnableSsl = true;
+        }
+
+        public bool MandarCorreo(String mensaje, String asunto, String destino, String correoRemitente, String nombreRemitente)
+        {
+            try
+            {               
+                MailMessage mnsj = new MailMessage();
+                mnsj.Subject = asunto;
+                mnsj.To.Add(new MailAddress(destino));
+                mnsj.From = new MailAddress(correoRemitente, nombreRemitente);
+                mnsj.Body = mensaje;
+                server.Send(mnsj);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false; 
+            }           
+        }
+    }
+}
